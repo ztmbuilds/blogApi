e391214898709dd46956c982796d9c4647303bbe
Zetro <s.zetro007@gmail.com>
2024-08-01 15:27:44 +0100

    update

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const { sequelize } = require('./models');
-const { initializeRedisClient } = require('./middlewares/redis');
-require('dotenv').config({ path: `${process.cwd()}/.env` });
-const app = require('./app');
+const { sequelize } = require("./models");
+const { initializeRedisClient } = require("./middlewares/redis");
+require("dotenv").config({ path: `${process.cwd()}/.env` });
+const app = require("./app");
 
 async function initializeExpressServer() {
   // connect to Redis
-  // await initializeRedisClient();
+  await initializeRedisClient();
 
   app.listen(process.env.PORT || 3000, async () => {
-    console.log('Server is running on port 5000');
+    console.log("Server is running on port 5000");
     await sequelize.authenticate();
-    console.log('Database Connected!');
+    console.log("Database Connected!");
   });
 }
 
